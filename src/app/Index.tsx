46c0757016eae77a3fa88a8f6f962cb94aee990a
Samuel Williams <samuelspexy@gmail.com>
2025-01-02 18:49:23 +0200

    Main View

---
+++ src/app/Index.tsx
@@ -0,0 +1,17 @@
+import { View, Text } from "react-native";
+import React from "react";
+import Name from "../components/Name";
+import Age from "../components/Age";
+import Xhandle from "../components/Xhandle";
+import Subject from "../components/Subject";
+import Card from "../components/Card";
+
+const Index = () => {
+  return (
+    <View>
+      <Card />
+    </View>
+  );
+};
+
+export default Index;
