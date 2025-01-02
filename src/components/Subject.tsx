b30753d1bf3879824e5ccfc99b86c2ddcb4e1c84
Samuel Williams <samuelspexy@gmail.com>
2025-01-02 18:04:44 +0200

    component challenge

---
+++ src/components/Subject.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Subject = () => {
+  return (
+    <View>
+      <Text>I love Math.</Text>
+    </View>
+  );
+};
+
+export default Subject;
---
+++ src/components/Xhandle.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Xhandle = () => {
+  return (
+    <View>
+      <Text>Here is X handle @samsam</Text>
+    </View>
+  );
+};
+
+export default Xhandle;
