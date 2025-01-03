2b78781bdff7096b1e4e680d629474a12e75d53c
Samuel Williams <samuelspexy@gmail.com>
2025-01-03 12:27:07 +0200

    style challenge

---
+++ src/utils/style.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  textContainer: {
+    padding: 20,
+    margin: 20,
+    shadowColor: "black",
+    shadowOpacity: 0.5,
+    shadowOffset: {
+      width: 6,
+      height: 6,
+    },
+    shadowRadius: 3,
+    borderWidth: 1,
+    borderRadius: 3,
+  },
+});
+
+export default styles;
